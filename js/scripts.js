570688078f880031eeef8f354d2eef5c6aa99b8f
Jaime Williams <jaime.williams@eagles.ewu.edu>
2014-03-06 17:56:00 -0800

    here we go

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 		$('.gallery div').addClass('large-3');
 
 		// Hide Paragraphs
-		$('p').hide();
+		// $('p').hide();
 
 	});
 
@@ -28,10 +28,139 @@ $(document).ready(function(){
 		$('.gallery div').addClass('large-6');
 
 		// Show paragraphs
-		$('p').show();
+		// $('p').show();
 
 	});
 
-	console.log('Page Loaded. Lets Do this!');
+	$("#featured").on("ready.fndtn.orbit", function(event) {
+	  console.info("ready");
+	});
+	$("#featured").on("before-slide-change.fndtn.orbit", function(event, orbit) {
+	  console.info("before slide change");
+	});  
 
+	$("#featured").on("after-slide-change.fndtn.orbit", function(event, orbit) {
+	  console.info("after slide change");
+	  console.info("slide " + orbit.slide_number + " of " + orbit.total_slides);
+
+		if (orbit.slide_number == 1) {
+		  	$( "p[data-orbit-link|='slide-1']" ).hide();
+		  	$( "p[data-orbit-link|='slide-2']" ).show( "slow" );
+		  	$( "a[data-orbit-link|='slide-2']" ).animate({
+					    opacity: 0.5,
+					    up: "+=.25",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  	$( "a[data-orbit-link|='slide-1']" ).animate({
+					    opacity: 1,
+					    up: "+= 0",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  	$( "p[data-orbit-link|='slide-3']" ).hide();
+		  	$( "p[data-orbit-link|='slide-4']" ).hide();
+		  	$( "p[data-orbit-link|='slide-5']" ).hide();
+	  }
+	  else if (orbit.slide_number == 2) {
+	  		$( "p[data-orbit-link|='slide-1']" ).hide();
+		  	$( "p[data-orbit-link|='slide-2']" ).hide();
+		  	$( "p[data-orbit-link|='slide-3']" ).show( "slow" );
+		  	$( "a[data-orbit-link|='slide-3']" ).animate({
+					    opacity: 0.5,
+					    up: "+=.25",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  	$( "a[data-orbit-link|='slide-2']" ).animate({
+					    opacity: 1,
+					    up: "+= 0",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  	$( "p[data-orbit-link|='slide-4']" ).hide();
+		  	$( "p[data-orbit-link|='slide-5']" ).hide();	  	
+	  }
+	  else if (orbit.slide_number == 3) {
+	  		$( "p[data-orbit-link|='slide-1']" ).hide();
+		  	$( "p[data-orbit-link|='slide-2']" ).hide();
+		  	$( "p[data-orbit-link|='slide-3']" ).hide();
+		  	$( "p[data-orbit-link|='slide-4']" ).show( "slow" );
+		  			  	$( "a[data-orbit-link|='slide-4']" ).animate({
+					    opacity: 0.5,
+					    up: "+=.25",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  			  			  	$( "a[data-orbit-link|='slide-3']" ).animate({
+					    opacity: 1,
+					    up: "+= 0",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  	$( "p[data-orbit-link|='slide-5']" ).hide();	  	
+	  }
+	  else if (orbit.slide_number == 4) {
+	  		$( "p[data-orbit-link|='slide-1']" ).hide();
+		  	$( "p[data-orbit-link|='slide-2']" ).hide();
+		  	$( "p[data-orbit-link|='slide-3']" ).hide();
+		  	$( "p[data-orbit-link|='slide-4']" ).hide();
+		  	$( "p[data-orbit-link|='slide-5']" ).show( "slow" );
+		  	$( "a[data-orbit-link|='slide-5']" ).animate({
+					    opacity: 0.5,
+					    up: "+=.25",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+				$( "a[data-orbit-link|='slide-4']" ).animate({
+					    opacity: 1,
+					    up: "+= 0",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});	  	
+	  }
+	  else {
+	  		$( "p[data-orbit-link|='slide-1']" ).show( "slow" );
+	  		$( "a[data-orbit-link|='slide-1']" ).animate({
+					    opacity: 0.5,
+					    up: "+=.25",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+	  		$( "a[data-orbit-link|='slide-5']" ).animate({
+					    opacity: 1,
+					    up: "+= 0",
+					    height: "slide"
+					  }, 100, function() {
+					    // Animation complete.
+					});
+		  	$( "p[data-orbit-link|='slide-2']" ).hide();
+		  	$( "p[data-orbit-link|='slide-3']" ).hide();
+		  	$( "p[data-orbit-link|='slide-4']" ).hide();
+		  	$( "p[data-orbit-link|='slide-5']" ).hide();	  	
+	  } 
+
+	});
+
+	$("#featured").on("timer-started.fndtn.orbit", function(event, orbit) {
+	  console.info("timer started");
+	});
+	$("#featured").on("timer-stopped.fndtn.orbit", function(event, orbit) {
+	  console.info("timer stopped");
+	});
+	console.log('Page Loaded. Lets Do this!');
+	$( "p[data-orbit-link|='slide-1']" ).show( "slow" );
+	$( "p[data-orbit-link|='slide-2']" ).hide();
+	$( "p[data-orbit-link|='slide-3']" ).hide();
+	$( "p[data-orbit-link|='slide-4']" ).hide();
+	$( "p[data-orbit-link|='slide-5']" ).hide();
 }); 
